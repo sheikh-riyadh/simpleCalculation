cd0f0ff28c6fce652594783b2e282312e4840ff5
Sheikh Riyadh <sheikhriyad350883@gmail.com>
2022-08-06 13:09:38 +0600

    Return money result

---
+++ simpleCalculation.js
@@ -0,0 +1,8 @@
+
+let harryAmount = 1000;
+let oranges = 400;
+let apple = 300;
+let totalBuy = oranges + apple;
+let returnMoney = harryAmount - totalBuy;
+
+console.log("Return of money's: ", returnMoney);
